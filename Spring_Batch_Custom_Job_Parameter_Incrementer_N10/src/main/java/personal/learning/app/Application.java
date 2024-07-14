b5719a951093eb986b8c7887f02f9f7e95441756
Aman Mishra <mishraamankpa@gmail.com>
2024-07-14 13:09:52 +0530

    Batch Job Param Incrementer

--- Spring_Batch_Custom_Job_Parameter_Incrementer_N10/src/main/java/personal/learning/app/Application.java
+++ Spring_Batch_Custom_Job_Parameter_Incrementer_N10/src/main/java/personal/learning/app/Application.java
@@ -1,16 +1,7 @@
 package personal.learning.app;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 /*
  * Directly run SpringBoot application
  * (Right Click on project >> Run As >> Spring Boot App)
